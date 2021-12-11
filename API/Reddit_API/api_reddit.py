f7503dc8aca6e54c7711c57d32f3d833bfd81014
ludovicChevallier <45288555+ludovicChevallier@users.noreply.github.com>
2021-12-11 13:39:37 +0100

    900-1000 records
    modify the function to take 10 subjects chosed as top trending and search for 100 records per subjects

--- API/Reddit_API/api_reddit.py
+++ API/Reddit_API/api_reddit.py
@@ -7,16 +7,27 @@ client_id="BPeqBX7V9aPBAQMIZQGHRQ"
 secret="S1RvpoYGAefvUGWZY4mPlIjldKZRBg"
 user_agent="SDE webscrapping"
 def search_post():
+    final_dataset=[]
     reddit = praw.Reddit(client_id=client_id, client_secret=secret, user_agent=user_agent)
     posts = []
     # if we want more info about the subrreddit (like description) https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html
-    ml_subreddit = reddit.subreddit('MachineLearning')
-    for post in ml_subreddit.hot(limit=10):
-        post.created=int(post.created)
-        timestamp = datetime.datetime.fromtimestamp(post.created)
-        new_datetime = timestamp.strftime('%Y-%m-%d')
-        #details about what kind of information we can find in post https://praw.readthedocs.io/en/latest/code_overview/models/submission.html?highlight=num_comments#praw.models.Submission
-        posts.append([post.id,new_datetime,post.title,post.author.name, post.score, post.subreddit.display_name, post.url, post.num_comments, post.selftext ])
-    posts = pd.DataFrame(data=posts,columns=['id', 'date', 'name','name_author','score', 'subreddit_name', 'url', 'num_comments', 'text'])
-    posts.to_json(r'./API/Reddit_API/post.json',orient='records',default_handler=str)
+    trending=["afghanistan","COVID19","FIFA22","dune","dogecoin","squidgame","sports","ethereum","tigerwoods","battlefield2042"]
+    for trends in trending:
+        print(trends)
+        ml_subreddit = reddit.subreddit(trends)
+        for post in ml_subreddit.hot(limit=100):
+            post.created=int(post.created)
+            timestamp = datetime.datetime.fromtimestamp(post.created)
+            new_datetime = timestamp.strftime('%Y-%m-%d')
+            name=""
+            if(post.author==None):
+                name=""
+            else:
+                name=post.author.name
+            #details about what kind of information we can find in post https://praw.readthedocs.io/en/latest/code_overview/models/submission.html?highlight=num_comments#praw.models.Submission
+            posts.append([post.id,trends,new_datetime,post.title,name, post.score, post.subreddit.display_name, post.url, post.num_comments, post.selftext ])
+        posts = pd.DataFrame(data=posts,columns=['id','subject', 'date', 'name','name_author','score', 'subreddit_name', 'url', 'num_comments', 'text'])
+        final_dataset.append(posts)
+    final_dataset=pd.concat(final_dataset)
+    final_dataset.to_json(r'./API/Reddit_API/post.json',orient='records',default_handler=str)
 search_post()
--- API/Twitter_API/api_twitter.py
+++ API/Twitter_API/api_twitter.py
@@ -10,26 +10,31 @@ BEARER_TOKEN="AAAAAAAAAAAAAAAAAAAAAO3AWgEAAAAA9xqjDcIkoan0PxrDixk6ayBO6SY%3DTd9k
 access_token="1468521297118478336-SFvUrKRb4394arDgqQjZUattq0Y7Ee"
 access_token_secret="6BddGxqoNdH1b6y8VQCz1uWgqGvDKv9kYKCCmKNWSjkwV"
 
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth = tw.OAuthHandler(consumer_key, consumer_secret,)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 #https://www.earthdatascience.org/courses/use-data-open-source-python/intro-to-apis/twitter-data-in-python/
 def search_tweet():
-    search_words = "#wildfires"
-    date_since = "2018-11-16"
-    #search only tweets about search words which are the most popular but can look for recent or mixed 
-    #we can look for tweets before a certain date with until
-    tweets = tw.Cursor(api.search_tweets,
-                q=search_words,
-                lang="en",
-                result_type="mixed").items(5)
-    data_sets=[]
-    for tweet in tweets:
-        #for more information look here: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets#example-response
-        new_datetime = tweet.created_at.strftime('%Y-%m-%d')
-        data_sets.append([tweet.id,new_datetime,tweet.user.name,tweet.user.location,tweet.lang, tweet.text])
-    tweet_text = pd.DataFrame(data=data_sets, 
-                    columns=['id', "date","user_name","location","language","text"])
-    tweet_text.to_json(r'./API/Twitter_API/tweets.json',orient='records')
+    trending=["#Afghanistan","#COVID","#FIFA22","#Dune","#AMC","#SquidGame","#T20worldcup","#Ethereum","#TigerWoods","#Batllefield2042"]
+    final_dataset=[]
+    for trends in trending:
+        search_words = trends
+        date_since = "2018-11-16"
+        #search only tweets about search words which are the most popular but can look for recent or mixed 
+        #we can look for tweets before a certain date with until
+        tweets = tw.Cursor(api.search_tweets,
+                    q=search_words,
+                    lang="en",
+                    result_type="mixed").items(100)
+        data_sets=[]
+        for tweet in tweets:
+            #for more information look here: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets#example-response
+            new_datetime = tweet.created_at.strftime('%Y-%m-%d')
+            data_sets.append([tweet.id,trends,new_datetime,tweet.user.name,tweet.user.location,tweet.lang, tweet.text])
+        tweet_text = pd.DataFrame(data=data_sets, 
+                        columns=['id','subject',"date","user_name","location","language","text"])
+        final_dataset.append(tweet_text)
+    final_dataset=pd.concat(final_dataset)
+    final_dataset.to_json(r'./API/Twitter_API/tweets.json',orient='records')
 
 search_tweet()
